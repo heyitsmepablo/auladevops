ffd1aca5ef0efb39b01f78c4518c9a8bd330225a
Pablo Eduardo <heyitsmepablo@github.com>
2024-03-20 15:56:22 -0300

    Aula 3 de Form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,8 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 
+
 export const routes: Routes = [
   {path:'login',component:LoginComponent},
-  {path:'',component:HomeComponent}
+  {path:'',component:HomeComponent},
 ];
